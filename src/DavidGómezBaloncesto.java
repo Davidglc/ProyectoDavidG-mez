eb67a2b606d47177f472f4a97e87542cc819fba8
Vespertino <Vespertino@C05PC22.informatica.comercio>
2022-02-21 20:21:56 +0100

    Primer commit

---
+++ src/DavidGómezBaloncesto.java
@@ -0,0 +1,16 @@
+
+public class DavidGómezBaloncesto{
+	public String titulo;
+	public int nPaginas;
+	public String nombreHobby;
+	
+	public DavidGómezBaloncesto(String titulo, int nPaginas, String nombreHobby) {
+		super();
+		this.titulo = titulo;
+		this.nPaginas = nPaginas;
+		this.nombreHobby = nombreHobby;
+	}
+	public String gettitulo() {
+		return titulo;
+	}
+}
